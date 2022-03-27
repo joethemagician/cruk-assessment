5ab043fb73ca2c7a8ea437c952c3df2c3be7c614
joethemagician <joe.stone@bbc.co.uk>
2022-03-27 18:14:13 +0100

    Add basic implementation for both lambdas and api gateway

--- lambda/donations.js
+++ lambda/donations.js
@@ -1,8 +1,74 @@
-exports.handler = async function(event) {
-    console.log("request:", JSON.stringify(event, undefined, 2));
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "text/plain" },
-      body: 'users'
+const AWS = require("aws-sdk");
+
+const dB = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+    const TABLE = 'Donations';
+    let body;
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json"
     };
-  };
+
+    let path = event.resource;
+    let httpMethod = event.httpMethod;
+    let route = httpMethod.concat(' ').concat(path);
+
+    try {
+        switch (route) {
+            case "GET /donations":
+                body = await dB.scan({TableName: TABLE}).promise();
+                break;
+            case "GET /donations/{id}":
+                body = await dB
+                    .get({
+                        TableName: TABLE,
+                        Key: {
+                            id: event.pathParameters.id
+                        }
+                    })
+                    .promise();
+                break;
+            case "PUT /donations":
+                let requestJSON = JSON.parse(event.body);
+                await dB
+                    .put({
+                        TableName: TABLE,
+                        Item: {
+                            id: requestJSON.id,
+                            email: requestJSON.email
+                        }
+                    })
+                    .promise();
+                body = `Put item ${requestJSON.id}`;
+                break;
+            case "DELETE /donations/{id}":
+                await dB
+                    .delete({
+                        TableName: TABLE,
+                        Key: {
+                            id: event.pathParameters.id
+                        }
+                    })
+                    .promise();
+                body = `Deleted item ${event.pathParameters.id}`;
+                break;
+
+            default:
+                throw new Error(`Unsupported route: "${route}"`);
+        }
+    } catch (err) {
+        console.log(err)
+        statusCode = 400;
+        body = err.message;
+    } finally {
+        console.log(body)
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers
+    };
+};
--- lib/cruk-assessment-stack.ts
+++ lib/cruk-assessment-stack.ts
@@ -1,22 +1,134 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {CfnOutput, Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {AttributeType, Table} from "aws-cdk-lib/aws-dynamodb";
+import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
+import {Policy, PolicyStatement} from "aws-cdk-lib/aws-iam";
+import {LambdaIntegration, RestApi} from "aws-cdk-lib/aws-apigateway";
 
 export class CrukAssessmentStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    private userTable: Table;
+    private donationTable: Table;
+    private usersLambda: Function;
+    private donationsLambda: Function;
+    private api: RestApi;
 
-    const users = new lambda.Function(this, 'UsersHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'uesrs.handler'
-    });
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    const donations = new lambda.Function(this, 'DonationsHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'donations.handler'
-    });
+        this.createTables();
 
-  }
+        this.createApiLambdas();
+
+        this.createApi();
+    }
+
+    /*
+        Creating a DynamoDB table for storing User data
+     */
+    private createTables = () => {
+        this.userTable = new Table(this, 'UsersTable', {
+            tableName: 'Users',
+            partitionKey: {
+                name: 'id',
+                type: AttributeType.STRING
+            },
+        });
+
+        this.donationTable = new Table(this, 'DonationsTable', {
+            tableName: 'Donations',
+            partitionKey: {
+                name: 'id',
+                type: AttributeType.STRING
+            },
+        });
+    }
+
+    /*
+        Creating lambda function for handling API endpoints
+     */
+    private createApiLambdas = () => {
+        this.usersLambda = new Function(this, 'User-Lambda', {
+            code: Code.fromAsset('lambda'),
+            handler: "users.handler",
+            runtime: Runtime.NODEJS_14_X
+        });
+
+        this.donationsLambda = new Function(this, 'Donation-Lambda', {
+            code: Code.fromAsset('lambda'),
+            handler: "donations.handler",
+            runtime: Runtime.NODEJS_14_X
+        });
+
+        //Required permissions for Lambda function to interact with User table
+        const tablePermissionPolicy = new PolicyStatement({
+            actions: [
+                "dynamodb:BatchGetItem",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:BatchWriteItem",
+                "dynamodb:PutItem",
+                "dynamodb:UpdateItem",
+                "dynamodb:DeleteItem"
+            ],
+            resources: [this.userTable.tableArn, this.donationTable.tableArn]
+        });
+
+        //Attaching an inline policy to the role
+        this.usersLambda.role?.attachInlinePolicy(
+            new Policy(this, `UserTablePermissions`, {
+                statements: [tablePermissionPolicy],
+            }),
+        );
+
+        this.donationsLambda.role?.attachInlinePolicy(
+            new Policy(this, `DonationsTablePermissions`, {
+                statements: [tablePermissionPolicy],
+            }),
+        );
+    }
+
+    /*
+        API Gateway integration
+     */
+    private createApi = () => {
+        this.api = new RestApi(this, `UsersDonationsAPI`, {
+            description: 'Users & Donations API',
+            deployOptions: {
+                stageName: 'test'
+            },
+            defaultCorsPreflightOptions: {
+                allowHeaders: [
+                    'Content-Type',
+                    'X-Amz-Date',
+                    'Authorization',
+                    'X-Api-Key',
+                ],
+                allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+                allowCredentials: true,
+                allowOrigins: ['http://localhost:3000'],
+            },
+        });
+        new CfnOutput(this, 'apiUrl', {value: this.api.url});
+
+        this.addApiResources();
+    }
+
+    private addApiResources = () => {
+        const users = this.api.root.addResource('users');
+        const user = users.addResource('{id}');
+
+        const donations = this.api.root.addResource('donations');
+        const donation = donations.addResource('{id}');
+
+        users.addMethod('GET', new LambdaIntegration(this.usersLambda));
+        users.addMethod('PUT', new LambdaIntegration(this.usersLambda));
+        user.addMethod('GET', new LambdaIntegration(this.usersLambda));
+        user.addMethod('DELETE', new LambdaIntegration(this.usersLambda));
+
+        donations.addMethod('GET', new LambdaIntegration(this.donationsLambda));
+        donations.addMethod('PUT', new LambdaIntegration(this.donationsLambda));
+        donation.addMethod('GET', new LambdaIntegration(this.donationsLambda));
+        donation.addMethod('DELETE', new LambdaIntegration(this.donationsLambda));
+    }
 }
