cb03d270acf46af314d7d361e817a956c1dc08e3
joethemagician <joe.stone@bbc.co.uk>
2022-04-02 15:35:08 +0100

    Remove user resources and lambdas

--- lib/cruk-assessment-stack.ts
+++ lib/cruk-assessment-stack.ts
@@ -1,6 +1,6 @@
 import {CfnOutput, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {AttributeType, Table} from "aws-cdk-lib/aws-dynamodb";
+import {AttributeType, ProjectionType, Table} from "aws-cdk-lib/aws-dynamodb";
 import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
 import {LambdaIntegration, RestApi} from "aws-cdk-lib/aws-apigateway";
 
@@ -11,7 +11,7 @@ export class CrukAssessmentStack extends Stack {
     private usersFetchLambda: Function;
     private usersUpdateLambda: Function;
     private usersDeleteLambda: Function;
-    private donationsLambda: Function;
+    private donationsCreateLambda: Function;
     private api: RestApi;
 
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -28,14 +28,6 @@ export class CrukAssessmentStack extends Stack {
         Creating a DynamoDB table for storing User data
      */
     private createTables = () => {
-        this.userTable = new Table(this, 'UsersTable', {
-            tableName: 'Users',
-            partitionKey: {
-                name: 'id',
-                type: AttributeType.STRING
-            },
-        });
-
         this.donationTable = new Table(this, 'DonationsTable', {
             tableName: 'Donations',
             partitionKey: {
@@ -43,37 +35,28 @@ export class CrukAssessmentStack extends Stack {
                 type: AttributeType.STRING
             },
         });
+
+        this.donationTable.addGlobalSecondaryIndex({
+            indexName: 'emailIndex',
+            partitionKey: {
+                name: 'email', 
+                type: AttributeType.STRING
+            },
+            projectionType: ProjectionType.ALL,
+        });
     }
 
     /*
         Creating lambda function for handling API endpoints
      */
     private createApiLambdas = () => {
-        this.usersListLambda = new Function(this, 'User-List-Lambda', {
-            code: Code.fromAsset('lambda/users'),
-            handler: "list.handler",
-            runtime: Runtime.NODEJS_14_X
-        });
-        this.usersFetchLambda = new Function(this, 'User-Fetch-Lambda', {
-            code: Code.fromAsset('lambda/users'),
-            handler: "fetch.handler",
-            runtime: Runtime.NODEJS_14_X
-        });
-        this.usersUpdateLambda = new Function(this, 'User-Update-Lambda', {
-            code: Code.fromAsset('lambda/users'),
-            handler: "update.handler",
-            runtime: Runtime.NODEJS_14_X
-        });
-        this.usersDeleteLambda = new Function(this, 'User-Delete-Lambda', {
-            code: Code.fromAsset('lambda/users'),
-            handler: "delete.handler",
+        this.donationsCreateLambda = new Function(this, 'Donation-Create-Lambda', {
+            code: Code.fromAsset('lambda/donations'),
+            handler: "create.handler",
             runtime: Runtime.NODEJS_14_X
         });
 
-        this.userTable.grantReadWriteData(this.usersListLambda);
-        this.userTable.grantReadWriteData(this.usersFetchLambda);
-        this.userTable.grantReadWriteData(this.usersUpdateLambda);
-        this.userTable.grantReadWriteData(this.usersDeleteLambda);
+        this.donationTable.grantReadWriteData(this.donationsCreateLambda);
                 
     }
 
@@ -81,8 +64,8 @@ export class CrukAssessmentStack extends Stack {
         API Gateway integration
      */
     private createApi = () => {
-        this.api = new RestApi(this, `UsersDonationsAPI`, {
-            description: 'Users & Donations API',
+        this.api = new RestApi(this, `DonationsAPI`, {
+            description: 'Donations API',
             deployOptions: {
                 stageName: 'test'
             },
@@ -104,20 +87,9 @@ export class CrukAssessmentStack extends Stack {
     }
 
     private addApiResources = () => {
-        const users = this.api.root.addResource('users');
-        const user = users.addResource('{id}');
-
-        // const donations = this.api.root.addResource('donations');
+        const donations = this.api.root.addResource('donations');
         // const donation = donations.addResource('{id}');
 
-        users.addMethod('GET', new LambdaIntegration(this.usersListLambda));
-        users.addMethod('PUT', new LambdaIntegration(this.usersUpdateLambda));
-        user.addMethod('GET', new LambdaIntegration(this.usersFetchLambda));
-        user.addMethod('DELETE', new LambdaIntegration(this.usersDeleteLambda));
-
-        // donations.addMethod('GET', new LambdaIntegration(this.donationsLambda));
-        // donations.addMethod('PUT', new LambdaIntegration(this.donationsLambda));
-        // donation.addMethod('GET', new LambdaIntegration(this.donationsLambda));
-        // donation.addMethod('DELETE', new LambdaIntegration(this.donationsLambda));
+        donations.addMethod('POST', new LambdaIntegration(this.donationsCreateLambda));
     }
 }
