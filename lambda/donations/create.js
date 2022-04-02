a4605de59120185eb8cec7b2f200d84d763b669f
joethemagician <joe.stone@bbc.co.uk>
2022-04-02 15:35:28 +0100

    Update create method for donations

--- lambda/donations/create.js
+++ lambda/donations/create.js
@@ -12,7 +12,8 @@ exports.handler = async (event, context) => {
     };
 
     try {
-        console.log('fetch',event.body);
+        // create the donation
+        console.log('create',event.body);
         let requestJSON = JSON.parse(event.body);
         let uuid = crypto.randomUUID();
         await db.put({
@@ -24,11 +25,32 @@ exports.handler = async (event, context) => {
                 }
             }).promise();
         body = `Put item ${uuid}`;
+
+        // check if second or later donation
+        let params = {
+            TableName: 'Donations',
+            IndexName: 'emailIndex',
+            FilterExpression: 'email = :email',
+            ExpressionAttributeValues: { ':email': requestJSON.email} 
+        }
+        console.log(params);
+        await db.scan(params, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            } else {
+                console.log(data);
+                if(data.Count >= 2){
+                    console.log('Thanks!!!!');
+                }
+            }
+        }).promise();
+    
+
     } catch (err) {
         console.log(err)
         statusCode = 400;
         body = err.message;
-    } finally {
+    } finally { 
         console.log(body)
         body = JSON.stringify(body);
     }
--- test/lambda/donations.test.ts
+++ test/lambda/donations.test.ts
@@ -7,6 +7,7 @@ const dummyDonation = {
     "email": "test.donation@test.com",
     "amount": 1000
 };
+let numberOfDonations = 1;
 
 jest.spyOn(crypto, 'randomUUID').mockReturnValue('32d7e58f-2e09-4982-b1bd-3d2f9316af27');
 
@@ -18,6 +19,13 @@ jest.mock('aws-sdk', () => {
                 return `Put item 32d7e58f-2e09-4982-b1bd-3d2f9316af27`
               }
             };
+        }),
+        scan: jest.fn().mockImplementation(() => {
+            return {
+              promise() {
+                return { Count: this.numberOfDonations }
+              }
+            };
         })
     };
     const mDynamoDB = { DocumentClient: jest.fn(() => mDocumentClient) };
@@ -26,21 +34,27 @@ jest.mock('aws-sdk', () => {
 
 const mDynamoDb = new aws.DynamoDB.DocumentClient();
 
-describe('Test get donations', () => {
+describe('Test create donation', () => {
     afterAll(() => {
       jest.resetAllMocks();
     });
 
-    it('should create / update donation', async () => {
+    it('should create donation', async () => {
         const response = await createDonation({ "body": JSON.stringify(dummyDonation)});
         expect(mDynamoDb.put).toBeCalledWith({
             TableName: 'Donations',
             Item: {
                 id: '32d7e58f-2e09-4982-b1bd-3d2f9316af27',
-                email: dummyDonation.email,
+                email: dummyDonation.email, 
                 amount: 1000
             }
         });
+        expect(mDynamoDb.scan).toBeCalledWith({
+            TableName: 'Donations',
+            IndexName: 'emailIndex',
+            FilterExpression: 'email = :email',
+            ExpressionAttributeValues: { ':email': dummyDonation.email }
+          }, expect.any(Function));
         expect(response.body).toEqual(`\"Put item 32d7e58f-2e09-4982-b1bd-3d2f9316af27\"`);
     });
    
